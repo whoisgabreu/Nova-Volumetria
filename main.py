692399553bccd3a43f4cfdc0a492ecb2c5453a02
Gabriel Lucas <gabreumartins02@gmail.com>
2025-02-19 16:46:51 -0300

    Finalizando sistema de coleta de dados volumetria

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from modules.get_shipmentId_sec import IdSecundaria
 from modules.get_scanned_amount_sec import CarregamentoSecundaria
 from modules.get_token import Get_Token
 
+import schedule
+import time
+
 # Módulo de login automático para coletar Authtoken
 # Editar o DotEnv incluindo o novo token
 
@@ -30,12 +33,25 @@ def update_dotenv(token):
 
     print("AUTHTOKEN atualizado com sucesso!")
 
-authtoken = Get_Token().start()
-if authtoken:
-    update_dotenv(authtoken)
+# 8, 12, 15 18 21
+def main_func():
+    authtoken = Get_Token().start()
+    if authtoken:
+        update_dotenv(authtoken)
 
-    IdTroncal().main()
-    IdSecundaria().main()
+        IdTroncal().main()
+        IdSecundaria().main()
 
-    CarregamentoTroncal().main()
-    CarregamentoSecundaria().main()
+        CarregamentoTroncal().main()
+        CarregamentoSecundaria().main()
+
+# Agendar a função para rodar nos horários especificados
+schedule.every().day.at("08:00").do(main_func)
+schedule.every().day.at("12:00").do(main_func)
+schedule.every().day.at("15:00").do(main_func)
+schedule.every().day.at("18:00").do(main_func)
+schedule.every().day.at("21:00").do(main_func)
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
